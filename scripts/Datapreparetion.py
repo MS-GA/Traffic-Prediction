f31e63c185ebe1164d3473c6a6b60d3af0306f14
M S <Malinisivakumar@yahoo.com>
2020-09-26 03:38:55 -0400

    First assignment

---
+++ scripts/Datapreparetion.py
@@ -0,0 +1,12 @@
+from IPython.display import Markdown
+## %% markdown
+## This project is about Traffic Prediction based on Collision data
+## %%
+
+import json
+import sys
+print('Python: {}'.format(sys.version))
+import pandas as pd
+
+import numpy as np
+print("Hello Capstone Project Course")
